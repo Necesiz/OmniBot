bf6485f4bbace31a63dffd91912602ff0f8e63bc
Necesiz <97000998+Necesiz@users.noreply.github.com>
2026-01-17 23:53:12 +0400

    Update start.py

--- Plugins/start.py
+++ Plugins/start.py
@@ -1,1 +1,15 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from asyncio import sleep
 
+async def start_message(message, client):
+    buttons = [
+        [InlineKeyboardButton("Əmrlər", callback_data="commands")],
+        [InlineKeyboardButton("Sahib", url="https://t.me/SAHIB_USERNAME")]
+    ]
+    sent = await message.reply_text(
+        "Salam! Mən OmniBot, qrupun idarəçisi və çoxfunksiyalı köməkçin! 🤖",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+    await sleep(3600)  # 1 saat
+    await sent.delete()
+    await message.delete()
