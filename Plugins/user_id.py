d2ab791ee848aa2aaf8669819985ccb4abf484b4
Necesiz <97000998+Necesiz@users.noreply.github.com>
2026-01-17 23:59:32 +0400

    Update user_id.py

--- Plugins/user_id.py
+++ Plugins/user_id.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+from pyrogram.types import Message
 
+async def show_id(message: Message, client: Client):
+    """
+    İstifadəçiyə öz Telegram ID-sini göstərir
+    """
+    user = message.from_user
+    if user:
+        text = f"Salam, {user.first_name}!\nSənin Telegram ID-n: `{user.id}`"
+        await message.reply_text(text, parse_mode="Markdown")
+    else:
+        await message.reply_text("ID-ni əldə edə bilmədim.")
